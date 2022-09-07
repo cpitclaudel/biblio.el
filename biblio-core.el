ba80c99a1b24fdcec4e3348ddda4bc04113ac17b
Fangyuan <wfy11235813@gmail.com>
2022-09-07 14:21:04 +0800

    use public API functions to check advices

--- biblio-core.el
+++ biblio-core.el
@@ -484,7 +484,9 @@ will be called with the metadata of the current item.")
 (defun biblio--completing-read-function ()
   "Return ido, unless user picked another completion package."
   (if (and (eq completing-read-function #'completing-read-default)
-           (not (advice--p (advice--symbol-function #'completing-read-default))))
+           (not (catch 'advised
+                  (advice-mapc (lambda (func plist) (throw 'advised t))
+                               'completing-read-default))))
       #'ido-completing-read
     completing-read-function))
 
